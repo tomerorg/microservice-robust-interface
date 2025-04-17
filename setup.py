c8466e3d99152a95103ca83126410df87ae8be1c
tomerbr3456 <59730830+tomerbr3456@users.noreply.github.com>
2025-04-17 13:25:00 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="microservice-robust-interface",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.1",
+        "pandas>=1.2.0",
+        "numpy>=1.20.0",
+    ],
+    author="",
+    author_email="",
+    description="NumPy and SQLAlchemy and Pandas based microservice interface in Python",
+    keywords="microservice-robust-interface, python",
+    url="",
+)
